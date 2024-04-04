84d3071c0e0e201db565f6855981306eebd32e10
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-04-04 10:49:25 +0700

    menentukan array yang dicari

--- LinearSearch/LinearSearch.cpp
+++ LinearSearch/LinearSearch.cpp
@@ -4,6 +4,10 @@
 #include <iostream>
 using namespace std;
 
+int arr[20];            //array to be searched
+int n;                  // number of element in the array
+int i;                  // index of array element
+
 int main()
 {
     
